648b74b35e031bdbbfd46b7294e7f9c8ed3a30ab
Baskoro <139613178+vntion@users.noreply.github.com>
2024-11-29 21:51:51 +0700

    update final ui 2

--- src/components/produk/ProdukCard.tsx
+++ src/components/produk/ProdukCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { formatRupiah } from "../../utils/helpers";
+import { formatRupiah, formatString } from "../../utils/helpers";
 
 interface Props {
   id: number;
@@ -10,26 +10,23 @@ interface Props {
 
 const ProductCard = ({ id, nama, harga, gambar }: Props) => {
   const formattedHarga = formatRupiah(harga);
+  const formattedNama = formatString(nama, 20);
 
   return (
     <Link
       to={`/produk/${id}`}
       className="group flex flex-col overflow-hidden rounded-lg bg-white shadow-sm transition-all hover:-translate-y-1 hover:border hover:border-primary hover:shadow-md"
     >
-      <div className="relative aspect-square bg-gray-100">
+      <div className="relative aspect-square">
         <img src={gambar} alt={nama} className="h-full w-full object-cover" />
       </div>
 
       <div className="flex flex-1 flex-col justify-between gap-3 p-4">
-        <h3 className="line-clamp-2 text-sm font-medium text-gray-900">
-          {nama}
-        </h3>
+        <h3 className="text-sm font-medium text-gray-900">{formattedNama}</h3>
 
-        <div className="mt-1">
-          <span className="text-lg font-semibold text-gray-900">
-            {formattedHarga}
-          </span>
-        </div>
+        <span className="text-lg font-semibold text-gray-900">
+          {formattedHarga}
+        </span>
       </div>
     </Link>
   );
